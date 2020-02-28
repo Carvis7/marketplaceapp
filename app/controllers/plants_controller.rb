d499e9b0c9fa7d1e7cf9750f6349da4a6be51421
Carvis7 <carlydavis91@gmail.com>
2020-02-28 19:46:39 +1100

    added user login and navbar

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,5 @@
 class PlantsController < ApplicationController
-
+  before_action :authenticate_user!, only: [:new,:create,:edit,:update,:destroy]
   before_action :find_plant, only: [:show,:edit,:update,:destroy]
 
   def index
